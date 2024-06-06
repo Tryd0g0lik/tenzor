8cf807a8ae446dee9ede34b9cace35091feec0a5
Tryd0g0lik <work80@mail.ru>
2024-06-06 12:35:49 +0700

    [master]: received four steps was

--- test_project/tenzor_pages.py
+++ test_project/tenzor_pages.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.abstract_event_listener import AbstractEventListener as AL
 from selenium.webdriver.support import expected_conditions as EC
+import json
 class TenzorPage:
 	def __init__(self, urls: str = 'https://sbis.ru/'):
 		self.urls = urls;
@@ -10,9 +12,9 @@ class TenzorPage:
 		self.found_selector = None
 
 	def run_browser(self) -> None:
-		# options = Options()
-		# options.add_argument('--headless')
-		self.driver = webdriver.Chrome() #.Chrome(options=options) #.Chrome()
+		options = Options()
+		options.add_argument('--headless')
+		self.driver = webdriver.Chrome(options=options) #.Chrome()
 
 	def open_page_byRef(self) -> None:
 		driver = self.driver
@@ -26,40 +28,77 @@ class TenzorPage:
 
 	def find_selector(self, fixture_selector, time=0.5):
 		driver = self.driver
-		driver.implicitly_wait(time)
 		found_selector = None
 
-		if self.urls.rfind('https://tensor.ru/') >= 0:
+		if ((self.urls.rfind('https://tensor.ru/') >= 0) or (self.urls.rfind('https://tensor.ru/about') >= 0)):
+			''' Step 3 & 4 & 5'''
+			hsize = 0
+			wsize = 0
 			driver.execute_script("window.scrollTo(0, window.innerHeight)")
-			# found_selector = WebDriverWait(driver, 10).until(
-			# 	EC.presence_of_element_located(*fixture_selector)
-			# )
-			found_selector = driver.find_element(*fixture_selector)
+			# driver.implicitly_wait(time)
+			# found_selector = driver.find_elements(*fixture_selector)
+			# found_selector = WebDriverWait(driver, timeout=time).until(EC.presence_of_element_located(fixture_selector))
 
+			#and (found_selector[len(found_selector) - 1].text.rfind('Подробнее') >= 0)
+			if ((fixture_selector[1]).rfind('p a[') >= 0):
+				''' ------ 3 & 4------ '''
+				# driver.implicitly_wait(time)
+				# self.make_click_byElement(found_selector[len(found_selector) - 1])
+				# driver.find_elements(*fixture_selector)[0].click()
+				# self.make_click_byElement(found_selector)
+				# driver.implicitly_wait(time)
+				try:
+					# wait = WebDriverWait(driver, timeout=time)
+					driver.execute_script("window.scrollTo(0, window.innerHeight)")
+					# found_selector = wait.until(EC.presence_of_element_located(fixture_selector)) .element_to_be_clickable
+					# wait.until(EC.presence_of_element_located(fixture_selector))
+					# (driver.find_element(*fixture_selector)).click()
+					elem = WebDriverWait(driver, timeout=time).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "p a[href='/about']")))
+					self.urls = elem.get_attribute('href')
+					self.open_page_byRef()
+					driver = self.driver
+					found_selector = driver.current_url
+				except Exception as e:
+					print(f"Error clicking on element: {e}")
+
+				# found_selector = driver.current_url
+				# if ((type(found_selector) == str) and (found_selector.text.rfind('Подробнее') >= 0)):
+				# if (type(found_selector) == str):
+
+
+
+			elif ((fixture_selector[1]).rfind(' img') >= 0):
+				''' ------ 5 ------ '''
+				for img in found_selector:
+					img
+			else:
+				# found_selector = found_selector[len(found_selector) - 1]
+				found_selector = WebDriverWait(driver, timeout=time).until(EC.presence_of_element_located(fixture_selector))
 			# found_selector = driver.find_element(*fixture_selector)
 			# (By.CLASS_NAME, "p.tensor_ru-Index__card-title.tensor_ru-pb-16")
-		if self.urls.rfind('https://sbis.ru/contacts/54-novosibirskaya-oblast') >= 0:
-			found_selector = driver.find_element(*fixture_selector)
+		elif self.urls.rfind('https://sbis.ru/contacts/54-novosibirskaya-oblast') >= 0:
+			''' Step 2'''
+			# found_selector = driver.find_element(*fixture_selector)
+			found_selector = WebDriverWait(driver, timeout=time).until(EC.presence_of_element_located(fixture_selector))
 
+		elif self.urls.rfind('https://sbis.ru/') >= 0:
+			''' Step 1'''
+			found_selector = WebDriverWait(driver, timeout=time).until(EC.presence_of_element_located(fixture_selector))
 
-
-
-		if self.urls.rfind('https://sbis.ru/') >= 0:
-			found_selector = driver.find_element(*fixture_selector)
-
-		driver.implicitly_wait(time)
 		self.driver = driver
-		return  found_selector
+		return found_selector
 
 	def teardown_method(self):
 		self.driver.close()
 
 	def move_by_link(self, selector):
-		self.found_selector = self.find_selector(selector)
-		if (len(self.found_selector.text) > 0 ):
-			text_found = (self.found_selector.text
-			              .strip()
-			              .lower())
-			return text_found
+		self.found_selector = self.find_selector(selector, 10)
+		text_found = ''
+		if (type(self.found_selector) != str ) and (len(self.found_selector.text) > 0 ):
+			text_found = (self.found_selector.text).strip().lower()
+		else:
+			text_found = (self.found_selector)
+
+		return text_found
 
 		return ''
--- test_project/test_tenzor.py
+++ test_project/test_tenzor.py
@@ -7,6 +7,8 @@ class Tenzor:
 	SELECTOR_TENZOR_ANCHOR_CONTAT = (By.CSS_SELECTOR, "div.sbisru-Header-sticky li a[href='/contacts']")
 	SELECTOR_TENZOR_ANCHOR_BANER = (By.CSS_SELECTOR, "a[href='https://tensor.ru/']")
 	SELECTOR_TENZOR_BOX_CONTENT = (By.CSS_SELECTOR, ".tensor_ru-Index__block4-bg p.tensor_ru-Index__card-title.tensor_ru-pb-16")
+	SELECTOR_TENZOR_BOX_CONTENT_LINK = (By.CSS_SELECTOR, "p a[href='/about']")
+	SELECTOR_TENZOR_BOX_WORKAHOLICS = (By.CSS_SELECTOR, ".tensor_ru-container.tensor_ru-section.tensor_ru-About__block3 img")
 
 
 # @pytest.mark.usefixtures("fixture_expected", "fixture_selector")
@@ -18,7 +20,7 @@ class TestTenzor():
 	# @pytest.mark.skipif(sys.tenzor.urls == "https://sbis.ru/", reason="Skipping test for specific URL")
 	def test_find_anchor(self):
 		extends: str = ''
-		# if self.tenzor.urls == "https://sbis.ru/":
+		''' Step 1'''
 		self.tenzor.open_page_byRef()
 		text_found = self.tenzor.move_by_link(Tenzor.SELECTOR_TENZOR_ANCHOR_CONTAT)
 		extends = "Контакты"
@@ -26,22 +28,21 @@ class TestTenzor():
 		assert text_found == extends.lower()
 		assert text_found.rfind(extends.lower()) >= 0
 
-		# self.tenzor.found_selector
-
+		''' Step 2'''
 		self.tenzor.urls = 'https://sbis.ru/contacts/54-novosibirskaya-oblast'
 		self.tenzor.make_click_byElement(self.tenzor.found_selector)
 		self.tenzor.driver.implicitly_wait(2)
 		self.tenzor.move_by_link(Tenzor.SELECTOR_TENZOR_ANCHOR_BANER)
 		self.tenzor.make_click_byElement(self.tenzor.found_selector)
+		''' Step 3'''
 		href = self.tenzor.found_selector.get_attribute('href')
 		self.tenzor.urls = 'https://tensor.ru/'
 
 		assert href == self.tenzor.urls.lower()
 		assert href.rfind(self.tenzor.urls.lower()) >= 0
 
-
+		self.tenzor.open_page_byRef()
 		if href.rfind(self.tenzor.urls.lower()) >= 0:
-			self.tenzor.open_page_byRef()
 			extends = "Сила в людях"
 
 			text_found = self.tenzor.move_by_link(Tenzor.SELECTOR_TENZOR_BOX_CONTENT)
@@ -50,12 +51,18 @@ class TestTenzor():
 			assert text_found == extends.lower()
 			assert text_found.rfind(extends.lower()) >= 0
 
+		if ((self.tenzor.urls).rfind('https://tensor.ru/') >= 0):
+			''' Step 4'''
+			self.tenzor.urls = 'https://tensor.ru/about'
+			self.tenzor.move_by_link(Tenzor.SELECTOR_TENZOR_BOX_CONTENT_LINK)# assert text_found.rfind(extends.lower()) >= 0
 
-
-
-
-
-
+		if (self.tenzor.urls.rfind('https://tensor.ru/about') >= 0):
+			''' Step 5'''
+			# self.tenzor.urls = 'https://tensor.ru/about'
+			# resultJS = self.tenzor.driver.execute_script("document.querySelectorAll('.tensor_ru-Index__block4-bg p')[document.querySelectorAll('.tensor_ru-Index__block4-bg p').length - 1]")
+			text_found = self.tenzor.move_by_link(Tenzor.SELECTOR_TENZOR_BOX_WORKAHOLICS)
+			assert text_found == extends.lower()
+			assert text_found.rfind(extends.lower()) >= 0
 
 
 def test_find_link_contacPage(browser):
