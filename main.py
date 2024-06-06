9bb185bee053c0a23f2d128dd2a022e4a90f83d5
Tryd0g0lik <work80@mail.ru>
2024-06-06 17:36:51 +0700

    [master]: completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pytest
 
 if __name__ == '__main__':
+    pytest.main(["-v", "--color=yes", "./test_project/test_tenzor.py"])
     pytest.main(["-v", "--color=yes", "./test_project/test_locations.py"])
 
 
--- test_project/test_locations.py
+++ test_project/test_locations.py
@@ -7,9 +7,6 @@ from test_project.interfaces import Refereces, Tenzor
 from test_project.tenzor_pages import TenzorPage
 import re
 
-
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 class TestLocation:
 
 	def check_location(self, selectors=Tenzor.SELECTOR_TENZOR_BOX_LOCATION, extends = 'Новосибирск') -> None:
@@ -17,8 +14,6 @@ class TestLocation:
 		if ((bool(found_selector.text)) and
 			(len(found_selector.text) > 0)):
 			text_found = found_selector.text.lower()
-
-			# assert text_found == extends.lower()
 			assert text_found.rfind(extends.lower()) >= 0
 
 
@@ -34,7 +29,6 @@ class TestLocation:
 		text =found_selector.text
 
 		try:
-			# elem = WebDriverWait(self.tenzor.driver, timeout=20).until(EC.element_located_selection_state_to_be((By.CSS_SELECTOR, 'span[title="Камчатский край"] span')))
 			actions = ActionChains(self.tenzor.driver)
 			actions.move_to_element(found_selector).click().perform()
 			WebDriverWait(self.tenzor.driver, 20).until(
@@ -45,8 +39,6 @@ class TestLocation:
 		result = re.split(r'\d+ ', text )
 		if ((len(result[1].split(' ')) > 0) and (len(result[1].split(' ')) > 1)):
 			result = [result[1].split(' ')[-2]]
-
-
 
 		self.check_location(extends=result[0])
 		self.tenzor.driver.close()
